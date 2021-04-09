6bf8729e3bb6859f2ca981429fb1447d5358ec74
shengjiangcong <jcsheng@ps-micro.com>
2021-04-09 20:46:17 +0800

    更新dh参数

--- xarm_ik/main.cpp
+++ xarm_ik/main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main()
 {
 	const double PI = 3.141592653;
-	float alength[6] = { 0, 0, 284.5, 0, 0, 0 };//DH Table -a
+	float alength[6] = { 53, 284.5, 80, 0, 0, 77 };//DH Table -a
 	float alpha[6] = { PI / 2, 0, PI / 2, -PI / 2, PI / 2, 0 };//DH Table -alpha
-	float dlength[6] = { 267, 0, 0, 207, 0, 439.5 };//DH Table -d
+	float dlength[6] = { 267, 0, 0, 342, 0, 96.5 };//DH Table -d
 	float theta0[6] = { 0 };//DH Table -theta
 	float angleOffset[6] = { 0, 0, -PI / 2, 0, -PI / 2, 0};
 	float highlim[6] = { 2 * PI, 2 * PI, 2 * PI, 2 * PI, 2 * PI, 2 * PI };
@@ -20,7 +20,17 @@ int main()
 	robc_ARM6DOF_set_movelim(highlim, lowlim);
 	robc_ARM6DOF_set_modle(0);
 	robc_ARM6DOF_set_jointweight(JointWeight);
-	float dest[6] = { 0.35, -0.64, 0, 0, 0, 0.52};
+	float dest[6] = { 0.8, -0.44, -1.23, 2.24, 2.25, 0};//0  0  -1.58  0  1.57  0
+	dest[1] = -1 * dest[1];
+	dest[2] = -1 * dest[2] - 1.58;
+	dest[4] = -1 * dest[4] + 1.57;
+
+
+	//d1 = j1
+	//d2 = -j2
+	//d3 = -1.58 - j3
+	//d4 = j4
+	//d5 = 1.57 - j5
 	robc_ARM6DOF_Anno_kicalc(dest);
 	/*float dest1[6] = { 225, 0, 460, 0, 0, -3.1415926 };
 
